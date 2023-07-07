e716e73c9ac01987a2f8b8458c3c9e3d8034cd05
MaouSama2802 <132946958+MaouSama2802@users.noreply.github.com>
2023-07-07 01:22:27 +0000

    c

--- Main.java
+++ Main.java
@@ -17,47 +17,48 @@ class Inventory {
 class Main {
 	public static void main(String[] args) {
 ArrayList<Inventory> elements = new ArrayList<>();
-		String userName="";
-		String userSerialNumber="";
-		int userValue=0;
-		Inventory userList = new Inventory(userName, userSerialNumber, userValue);
+String tempName;
+String tempSerial;
+int tempVal;
 Scanner in = new Scanner(System.in);
 int input=0;
 while(input!=5){
 	questions();
 	input=in.nextInt();
+	in.nextLine();
 	if(input==1){
 		System.out.println("Enter the name:");
-        userList.name=inputNewItemName();
-
+        tempName=in.nextLine();
 		System.out.println("Enter the serial number:");
-		userList.serialNumber=inputNewItemSerial();
+		tempSerial=in.nextLine();
 		System.out.println("Enter the value in dollars (whole number):");
-		userList.value=inputNewItemValue();
-		elements.add(new Inventory(userList.name,userList.serialNumber,userList.value));
+		tempVal=in.nextInt();
+		in.nextLine();
+		elements.add(new Inventory(tempName,tempSerial,tempVal));
 	}
 	else if(input==2){
 		System.out.println("Enter the serial number of the item to delete:");
-		userList.serialNumber=inputNewItemSerial();
+		tempSerial= in.nextLine();
 		for(int i=0; i< elements.size();i++) {
-			Inventory currentItem = elements.get(i);
-			if(currentItem.serialNumber.equals(userList.serialNumber)) {
+			if(elements.get(i).serialNumber.equals(tempSerial)) {
 				elements.remove(i);
 				break;
 			}
 		}
 	}
-	else if(input == 3){
+	else if(input == 3) {
 		System.out.println("Enter the serial number of the item to change:");
-        userList.serialNumber= in.next();
-		for (Inventory currentItem : elements) {
-			if (currentItem.serialNumber.equals(userList.serialNumber)) {
-				System.out.println("Enter the new name:");
-				userList.name = inputNewItemName();
-				System.out.println("Enter the new value in dollars (whole numbers):");
-				userList.value = inputNewItemValue();
-				currentItem.name = userList.name;
-				currentItem.value = userList.value;
+		tempSerial = in.next();
+		in.nextLine();
+		System.out.println("Enter the new name:");
+		tempName = in.nextLine();
+		System.out.println("Enter the new value in dollars (whole numbers):");
+		tempVal = in.nextInt();
+		for (Inventory element : elements) {
+			if (element.serialNumber.equals(tempSerial)) {
+				element.name = tempName;
+				element.value = tempVal;
+				break;
 			}
 		}
 	}
@@ -67,28 +68,7 @@ while(input!=5){
 		}
 	}
 }
-
 	}
-public static String inputNewItemName(){
-		String itemName;
-		Scanner scan = new Scanner(System.in);
-		itemName=scan.nextLine();
-		return itemName;
-}
-	public static String inputNewItemSerial(){
-		String itemSerialNumber;
-		Scanner scan = new Scanner(System.in);
-		itemSerialNumber=scan.nextLine();
-		return itemSerialNumber;
-	}
-	public static int inputNewItemValue(){
-		int itemValue;
-		Scanner scan = new Scanner(System.in);
-		itemValue=scan.nextInt();
-		return itemValue;
-	}
-
-
 	public static void questions(){
 		System.out.println("Press 1 to add an item.");
 		System.out.println("Press 2 to delete an item.");
